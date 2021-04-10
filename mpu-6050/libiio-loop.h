b7204209aca1ac224c5298aac219ebf96e9ff47c
Rahul Arasikere <arasikere.rahul@gmail.com>
2021-04-10 17:56:51 -0400

    rollback

--- mpu-6050/libiio-loop.h
+++
@@ -1,6 +0,0 @@
-#ifndef __ICM20608_LOOP_H__
-#define __ICM20608_LOOP_H__
-
-int libiio_loop(const char *uri, const char *device);
-
-#endif
--- mpu-6050/local-loop.h
+++
@@ -1,6 +0,0 @@
-#ifndef __ICM20608_LOCAL_LOOP_H_
-#define __ICM20608_LOCAL_LOOP_H_
-
-int local_loop(const char *device);
-
-#endif
